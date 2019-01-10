67c0eff165bce379749a51e07ad90c430fb425af
git-palace <edeveloper4@gmail.com>
2019-01-10 04:04:16 -0800

    new-campaign

--- includes/crm/emarketing.php
+++ includes/crm/emarketing.php
@@ -50,3 +50,216 @@ add_shortcode( 'email-campaign-list', function() {
 
 	return $template;
 } );
+
+add_shortcode( 'new-email-campaign', function() {
+    wp_enqueue_media();
+
+    $action = isset( $_GET['action'] ) ? $_GET['action'] : 'list';
+
+    if ( $action == 'edit' ) {
+        $campaign_id = !empty( $_GET['id'] ) ? absint( $_GET['id'] ) : 0;
+    }
+
+    $ecampGlobal = [
+        'ajaxurl'   => admin_url( 'admin-ajax.php' ),
+        'nonce'     => wp_create_nonce( 'erp-email-campaign' ),
+        'debug'     => defined( 'SCRIPT_DEBUG' ) ? SCRIPT_DEBUG : false,
+        'date'      => [
+            'format'        => ecamp_js_date_format(),
+            'placeholder'   => erp_format_date( 'now' )
+        ],
+        'time'      => [
+            'format'        => get_option( 'time_format', 'g:i a' ),
+            'placeholder'   => date( get_option( 'time_format', 'g:i a' ), current_time( 'timestamp' ) )
+        ],
+    ];
+
+    wp_enqueue_style( 'tiny-mce', site_url( '/wp-includes/css/editor.css' ), [], WPERP_EMAIL_CAMPAIGN_VERSION );
+    wp_enqueue_script( 'tiny-mce', site_url( '/wp-includes/js/tinymce/tinymce.min.js' ), [] );
+
+    wp_enqueue_script( 'tiny-mce-code', WPERP_EMAIL_CAMPAIGN_ASSETS . '/js/tinymce/plugins/code/plugin.min.js', [ 'tiny-mce' ], WPERP_EMAIL_CAMPAIGN_VERSION, true );
+    wp_enqueue_script( 'tiny-mce-hr', WPERP_EMAIL_CAMPAIGN_ASSETS . '/js/tinymce/plugins/hr/plugin.min.js', [ 'tiny-mce' ], WPERP_EMAIL_CAMPAIGN_VERSION, true );
+    wp_enqueue_script( 'tiny-mce-wpeditimage', site_url( '/wp-includes/js/tinymce/plugins/wpeditimage/plugin.min.js' ), [ 'tiny-mce' ], WPERP_EMAIL_CAMPAIGN_VERSION, true );
+
+    wp_enqueue_style( 'erp-email-campaign-template-style', WPERP_EMAIL_CAMPAIGN_ASSETS . '/css/email-template-styles.css', [], WPERP_EMAIL_CAMPAIGN_VERSION );
+    wp_enqueue_style( 'erp-email-campaign-editor' );
+
+    // scripts
+    wp_enqueue_script( 'erp-email-campaign-editor' );
+
+    wp_localize_script( 'erp-email-campaign-editor', 'ecampGlobal', $ecampGlobal );
+
+
+    wp_enqueue_style( 'erp-email-campaign-vendor' );
+
+    wp_enqueue_style( 'erp-email-campaign' );
+
+    wp_enqueue_script( 'erp-vue-table', WPERP_CRM_ASSETS . "/js/vue-table.js", [ 'erp-vuejs', 'jquery' ], WPERP_EMAIL_CAMPAIGN_VERSION, true );
+    wp_enqueue_script( 'erp-email-campaign-vendor' );
+    wp_localize_script( 'erp-vue-table', 'wpVueTable', [
+        'ajaxurl' => admin_url( 'admin-ajax.php' ),
+        'nonce'   => wp_create_nonce( 'wp-erp-vue-table' )
+    ] );
+
+    wp_enqueue_script( 'erp-email-campaign' );
+
+    // localized vars for the single campaign page
+    $ecampGlobal['searchPlaceHolder'] = __( 'Search Contact', 'erp-email-campaign' );
+
+    wp_localize_script( 'erp-email-campaign', 'ecampGlobal', $ecampGlobal );
+    
+    wp_enqueue_style( 'emarketing' );
+
+    ob_start();
+?>
+<div class="wrap erp-email-campaign erp-email-campaign-edit" id="erp-email-campaign-edit" v-cloak>
+
+    <h2 class="clear ecamp-page-title">
+        {{ pageTitle }}
+        <span class="alignright">
+            <button class="button" :disabled="isPreviewBtnDisabled" v-on:click="goToPreviewPage">
+                <i class="fa fa-eye"></i> {{ i18n.previewTemplate }}
+            </button>
+            <button class="button" :disabled="isPreviewBtnDisabled" v-on:click="sendPreviewEmail">
+                <i class="fa fa-paper-plane-o"></i> {{ i18n.sendPreview }}
+            </button>
+        </span>
+    </h2>
+
+    <form action="#" v-on:submit="preventFormSubmission">
+        <div class="erp-grid-container margin-top-12">
+            <div id="editor-step-1" v-if="1 === step">
+                <campaign-form  :i18n="i18n" :form-data="formData" :automatic-actions="automaticActions" :shortcodes="customizerData.shortcodes"></campaign-form>
+            </div>
+
+            <div id="editor-step-2" v-if="2 === step">
+                <customizer :customizer-data="customizerData" :i18n="i18n" :email-template="emailTemplate" ></customizer>
+            </div>
+
+            <div id="editor-step-3" v-if="3 === step">
+                <h3>{{ i18n.reviewDetails }}</h3>
+                <hr>
+                <table class="form-table review-details-table">
+                    <tbody>
+                        <tr>
+                            <th><label for="email-subject">{{ i18n.emailSubject }}</label></th>
+                            <td>{{ formData.subject }}</td>
+                        </tr>
+                        <tr>
+                            <th><label for="sender">{{ i18n.sender }}</label></th>
+                            <td>{{ formData.sender.name }} <{{ formData.sender.email }}></td>
+                        </tr>
+                        <tr>
+                            <th><label for="reply-to">{{ i18n.replyTo }}</label></th>
+                            <td>{{ formData.replyTo.name }} <{{ formData.replyTo.email }}></td>
+                        </tr>
+                        <tr>
+                            <th><label for="campaign-type">{{ i18n.newsletterType }}</label></th>
+                            <td>
+                                <span v-if="'automatic' !== formData.send">{{ i18n.standard }}</span>
+                                <span v-else>{{ i18n.automatic }}</span>
+
+                                <p v-if="'automatic' === formData.send">{{{ automaticPhrase }}}</p>
+                            </td>
+                        </tr>
+                        <tr v-if="'automatic' !== formData.send">
+                            <th><label>{{ i18n.lists }}</label></th>
+                            <td>
+                                <div class="row">
+                                    <div class="col-3">
+                                        <ul class="ecamp-contact-lists" v-for="(typeSlug, listType) in formData.lists">
+                                            <li><strong>{{ listType.title }}</strong></li>
+                                            <ul>
+                                                <li v-for="list in listType.lists" v-if="isListSelected(typeSlug, list.id)">
+                                                    {{ list.name }} ({{ list.count }})
+                                                </li>
+                                                <li v-if="!listType.lists.length"><em>{{ i18n.noListFound }}</em></li>
+                                            </ul>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr v-if="'automatic' !== formData.send">
+                            <th><label>{{ i18n.scheduleCampaign }}</label></th>
+                            <td>
+                                <div class="row erp-email-campaign-schedule-setter">
+                                    <div class="col-3" v-if="formData.isScheduled">
+                                        <div class="row">
+                                            <div class="col-1">
+                                                <input type="checkbox" class="form-control" v-model="formData.isScheduled">
+                                            </div>
+                                            <div class="col-2">
+                                                <datepicker
+                                                    id="delivery-date"
+                                                    class="form-control margin-bottom-12"
+                                                    :date="formData.schedule.date"
+                                                    :exclude="'prev'"
+                                                ></datepicker>
+                                            </div>
+
+                                            <div class="col-1 symbol">
+                                                @
+                                            </div>
+
+                                            <div class="col-2">
+                                                <timepicker
+                                                    id="delivery-time"
+                                                    class="form-control"
+                                                    :time="formData.schedule.time"
+                                                ></timepicker>
+                                            </div>
+                                        </div>
+
+                                        <p class="hint">Current local time is {{ currentLocalTime }}</p>
+
+                                    </div>
+                                    <div class="col-4" v-else>
+                                        <label><input type="checkbox" class="form-control" v-model="formData.isScheduled"> Yes schedule it</label>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><label>{{ i18n.googleCampaignName }}</label></th>
+                            <td>
+                                <div class="row">
+                                    <div class="col-3">
+                                        <input type="text" class="form-control" v-model="formData.campaignName">
+                                        <p class="hint">For example "New year sale"</p>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div><!-- .erp-grid-container -->
+
+        <p v-if="!hideFooterBtns">
+            <button class="button button-primary" type="button" v-if="showPrevBtn" v-on:click="--step">
+                <i class="fa fa-angle-double-left"></i> {{ i18n.previous }}
+            </button>
+             <button class="button button-primary" type="button" v-if="showNextBtn" :disabled="isNextBtnDisabled" v-on:click="goToNextStep(step)">
+                {{ i18n.next }} <i class="fa fa-angle-double-right"></i>
+            </button>
+             <button class="button button-primary" type="button" v-if="showSubmitBtn" :disabled="isSubmitBtnDisabled" v-on:click="saveCampaign(false, false)">
+                {{ submitBtnLabel }}
+            </button>
+             <button class="button" type="button" v-if="showSubmitBtn" :disabled="isSubmitBtnDisabled" v-on:click="saveCampaign(true, false)">
+                {{ i18n.saveAsDraft }}
+            </button>
+        </p>
+
+        <input type="hidden" name="campaign_id" value="<?php echo $campaign_id; ?>">
+    </form>
+
+</div><!-- #erp-email-campaign-edit -->
+
+
+<?php
+    $template = ob_get_contents();
+    ob_end_clean();
+
+    return $template;
+} );
--- index.php
+++ index.php
@@ -13,36 +13,37 @@ add_action( 'wp_enqueue_scripts', function() {
 
     foreach ( $css_files as $type => $files ) {
         foreach ( $files as $file ) {
-    	   wp_register_style( $file, plugins_url( '/assets/css/' . $type . '/' . $file . '.css', __FILE__ ), false );
+           wp_register_style( $file, plugins_url( '/assets/css/' . $type . '/' . $file . '.css', __FILE__ ), false );
         }
     }
 
+    $WP_ERP_MODULES_URL = WPERP_URL . '/modules';
+
     // global scripts
+    wp_register_style( 'erp-select2', WPERP_ASSETS . '/vendor/select2/select2.min.css', false, '1.0.0' );
     wp_register_script( 'erp-select2',  WPERP_ASSETS . '/vendor/select2/select2.full.min.js', array( 'jquery' ), '1.0.0', true );
 
     // js resources for schedules calendar
-	wp_register_script( 'erp-momentjs', WPERP_ASSETS . '/vendor/moment/moment.min.js', false, '1.0.0', true );
-	wp_register_script( 'erp-popup', WPERP_ASSETS . '/js/jquery-popup.js', array( 'jquery' ), '1.0.0', true );
+    wp_register_script( 'erp-momentjs', WPERP_ASSETS . '/vendor/moment/moment.min.js', false, '1.0.0', true );
+    wp_register_script( 'erp-popup', WPERP_ASSETS . '/js/jquery-popup.js', array( 'jquery' ), '1.0.0', true );
+    
+    wp_register_style( 'erp-fullcalendar', WPERP_ASSETS . '/vendor/fullcalendar/fullcalendar.min.css', false, '1.0.0' );
     wp_register_script( 'erp-fullcalendar', WPERP_ASSETS . '/vendor/fullcalendar/fullcalendar.min.js', array( 'jquery', 'erp-momentjs' ), '1.0.0', true );
+
     wp_register_script( 'erp-js', WPERP_ASSETS . '/js/erp.min.js', array( 'jquery', 'erp-fullcalendar', 'backbone', 'underscore', 'wp-util', 'jquery-ui-datepicker', 'erp-select2' ), '1.0.0', true );
 
-    // css for schedules calendar
-    wp_register_style( 'erp-fullcalendar', WPERP_ASSETS . '/vendor/fullcalendar/fullcalendar.min.css', false, '1.0.0' );
-    wp_register_style( 'erp-select2', WPERP_ASSETS . '/vendor/select2/select2.min.css', false, '1.0.0' );
-
     // js resources for contacts list table
-    $WP_ERP_MODULES_URL = WPERP_URL . '/modules';
     wp_register_script( 'erp-vuejs', WPERP_ASSETS . '/vendor/vue/vue.min.js', array( 'jquery' ), '1.0.0', true );
     wp_register_script( 'erp-vue-table', $WP_ERP_MODULES_URL . "/crm/assets/js/vue-table.js", array( 'erp-vuejs', 'jquery' ), date( 'Ymd' ), true );
     wp_register_script( 'erp-script', WPERP_ASSETS . '/js/erp.min.js', array( 'jquery', 'backbone', 'underscore', 'wp-util', 'jquery-ui-datepicker', 'erp-select2' ), '1.0.0', true );
-    wp_register_script( 'erp-tiptip', WPERP_ASSETS . '/vendor/tiptip/jquery.tipTip.min.js', array( 'jquery' ), '1.0.0', true );
     wp_register_script( 'erp-crm-contact', $WP_ERP_MODULES_URL . "/crm/assets/js/crm-contacts.js", array( 'erp-vue-table', 'erp-script', 'erp-vuejs', 'underscore', 'erp-tiptip', 'jquery', 'erp-select2' ), date( 'Ymd' ), true );
 
-    // css for contact list table
     wp_register_style( 'erp-tiptip', WPERP_ASSETS . '/vendor/tiptip/tipTip.css', false, '1.0.0' );
+    wp_register_script( 'erp-tiptip', WPERP_ASSETS . '/vendor/tiptip/jquery.tipTip.min.js', array( 'jquery' ), '1.0.0', true );
 
     wp_register_script( 'erp-timepicker', WPERP_ASSETS . '/vendor/timepicker/jquery.timepicker.min.js', array( 'jquery', 'erp-momentjs' ), date( 'Ymd' ), true );
     wp_register_style( 'erp-timepicker', WPERP_ASSETS . '/vendor/timepicker/jquery.timepicker.css', false, date( 'Ymd' ) );
+    
     wp_register_script( 'erp-crm', $WP_ERP_MODULES_URL . "/crm/assets/js/crm.js", array( 'erp-script', 'erp-timepicker' ), date( 'Ymd' ), true );
 
     // for activities
@@ -58,9 +59,50 @@ add_action( 'wp_enqueue_scripts', function() {
     wp_register_style( 'erp-sweetalert', WPERP_ASSETS . '/vendor/sweetalert/sweetalert.css', false, '1.4.1' );
     wp_register_script( 'erp-sweetalert', WPERP_ASSETS . '/vendor/sweetalert/sweetalert.min.js', array( 'jquery' ), '1.4.1', true );
 
-  // calendar
+    // calendar
     wp_register_script( 'erp-trix-editor', WPERP_ASSETS . '/vendor/trix/trix.js', array( 'jquery' ), date( 'Ymd' ), true );
     wp_register_style( 'erp-trix-editor', WPERP_ASSETS . '/vendor/trix/trix.css', false, date( 'Ymd' ) );
+
+    // email campaign
+    wp_register_style( 'erp-styles', WPERP_ASSETS . '/css/admin.css', false, date( 'Ymd' ) );
+    wp_register_style( 'erp-fontawesome', WPERP_ASSETS . '/vendor/fontawesome/font-awesome.min.css', false, date( 'Ymd' ) );
+
+    wp_register_script( 'erp-sortable', WPERP_EMAIL_CAMPAIGN_ASSETS . '/js/Sortable.js', [], WPERP_EMAIL_CAMPAIGN_VERSION, true );
+
+    wp_register_script( 'wp-color-picker', admin_url( '/js/color-picker.min.js' ), [], WPERP_EMAIL_CAMPAIGN_VERSION, true );
+
+    wp_register_style(
+        'erp-email-campaign-editor',
+        WPERP_EMAIL_CAMPAIGN_ASSETS . '/css/erp-email-campaign-editor.css', [
+        'wp-color-picker', 'erp-styles', 'erp-timepicker',
+        'erp-fontawesome', 'erp-sweetalert', 'erp-nprogress', 'erp-email-campaign-template-style'
+    ], WPERP_EMAIL_CAMPAIGN_VERSION );
+    wp_register_script(
+        'erp-email-campaign-editor',
+        WPERP_EMAIL_CAMPAIGN_ASSETS . '/js/erp-email-campaign-editor.js', [
+        'erp-vuejs', 'jquery', 'jquery-ui-datepicker', 'wp-color-picker',
+        'erp-timepicker', 'erp-sortable', 'erp-tiptip', 'erp-select2', 'erp-sweetalert', 'erp-nprogress'
+    ], WPERP_EMAIL_CAMPAIGN_VERSION, true );
+
+    wp_register_script( 'erp-flotchart',  WPERP_ASSETS . '/vendor/flot/jquery.flot.min.js', array( 'jquery' ), date( 'Ymd' ), true );
+    wp_register_script( 'erp-flotchart-pie', WPERP_ASSETS . '/vendor/flot/jquery.flot.pie.min.js', array( 'jquery' ), date( 'Ymd' ), true );
+
+    wp_register_style( 'erp-flotchart-valuelabel-css', WPERP_ASSETS . '/vendor/flot/plot.css', false, date( 'Ymd' ) );
+
+    wp_register_style( 'erp-email-campaign-vendor', WPERP_EMAIL_CAMPAIGN_ASSETS . '/css/erp-email-campaign-vendor.css', [], WPERP_EMAIL_CAMPAIGN_VERSION );
+    wp_register_script( 'erp-email-campaign-vendor', WPERP_EMAIL_CAMPAIGN_ASSETS . "/js/erp-email-campaign-vendor.js", [ 'jquery' ], WPERP_EMAIL_CAMPAIGN_VERSION, true );
+
+    wp_register_style(
+        'erp-email-campaign',
+        WPERP_EMAIL_CAMPAIGN_ASSETS . '/css/erp-email-campaign.css',
+        [ 'erp-styles', 'erp-nprogress', 'erp-flotchart-valuelabel-css', 'erp-email-campaign-vendor' ],
+        WPERP_EMAIL_CAMPAIGN_VERSION
+    );
+    wp_register_script( 
+        'erp-email-campaign', 
+        WPERP_EMAIL_CAMPAIGN_ASSETS . '/js/erp-email-campaign.js', [ 
+            'jquery', 'erp-vuejs', 'erp-nprogress', 'erp-flotchart', 'erp-flotchart-pie', 'erp-tiptip', 'erp-email-campaign-vendor', 'erp-momentjs' 
+    ], WPERP_EMAIL_CAMPAIGN_VERSION, true );
 
     // for global
     wp_enqueue_script( 'erp-select2' );
