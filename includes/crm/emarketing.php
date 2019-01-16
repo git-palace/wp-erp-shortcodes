d33d945078b041426de21a09f8025d24e08d462e
git-palace <edeveloper4@gmail.com>
2019-01-16 02:34:05 -0800

    fix pagination in wp list table

--- includes/crm/emarketing.php
+++ includes/crm/emarketing.php
@@ -12,6 +12,17 @@ add_shortcode( 'email-campaign-list', function() {
 	   require_once( ABSPATH . 'wp-admin/includes/template.php' );
 	}
 
+    if(!isset($_REQUEST['paged'])) {
+        $_REQUEST['paged'] = explode('/page/', $_SERVER['REQUEST_URI'], 2);
+        if(isset($_REQUEST['paged'][1])) list($_REQUEST['paged'],) = explode('/', $_REQUEST['paged'][1], 2);
+        if(isset($_REQUEST['paged']) and $_REQUEST['paged'] != '') {
+            $_REQUEST['paged'] = intval($_REQUEST['paged']);
+            if($_REQUEST['paged'] < 2) $_REQUEST['paged'] = '';
+        } else {
+            $_REQUEST['paged'] = '';
+        }
+    }
+
 	$campaign_table = new WeDevs\ERP\CRM\EmailCampaign\Campaign_List_Table();
 
 	ob_start();
