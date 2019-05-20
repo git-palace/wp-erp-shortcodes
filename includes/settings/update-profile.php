c1d73feaef4d0e139a9fc0e113d483ab12119b31
agentcloudre <info@agentcloud.com>
2019-05-20 11:04:51 +0000

    update

--- includes/settings/update-profile.php
+++ includes/settings/update-profile.php
@@ -60,10 +60,10 @@ add_shortcode( 'ac-update-profile', function() {
 		<?php wp_nonce_field( 'update-profile' ) ?>
 		<button id="submit" type="submit" disabled="">Update</button>
 		<?php 
-			if ( class_exists( 'ACGoogleSSOGmail' ) ) {
+			/* if ( class_exists( 'ACGoogleSSOGmail' ) ) {
 				$ACGoogleSSOGmail = new ACGoogleSSOGmail();
 				echo $ACGoogleSSOGmail->generate_sso_button();
-			}
+			} */
 		?>
 	</form>
 <?php
